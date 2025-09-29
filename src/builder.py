a442a0294a5d85dcb547b70ca32b4ef0ef9112ee
johnclamy <johnclamy@gmail.com>
2025-09-29 18:06:58 +0100

    create the enum types

---
+++ src/builder.py
@@ -0,0 +1,12 @@
+from enum import Enum
+
+
+class Builder(Enum):
+    FENDER = "Fender"
+    MARTIN = "Martin"
+    GIBSON = "Gibson"
+    COLLINGS = "Collings"
+    OLSON = "Olson"
+    RYAN = "Ryan"
+    PRS = "PRS"
+    ANY = "Any"
---
+++ src/guitar_type.py
@@ -0,0 +1,7 @@
+from enum import Enum
+
+
+class GuitarType(Enum):
+    ACOUSTIC = "Acoustic"
+    ELECTRIC = "Electric"
+    BASS = "Bass"
